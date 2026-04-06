953fd54ce92176246bc2710fb0702befd5b0f30a
LilCSharp <caleb.li.wheeler@gmail.com>
2026-04-05 22:45:16 -0400

    Added dependencies and test utilities for logging

--- services/stream-gateway/src/main.rs
+++ services/stream-gateway/src/main.rs
@@ -1,9 +1,15 @@
 use std::time::Duration;
 
+use log::info;
+
 use rdkafka::config::ClientConfig;
 use rdkafka::message::{Header, OwnedHeaders};
 use rdkafka::producer::{FutureProducer, FutureRecord};
-// use rdkafka::util::get_rdkafka_version;
+use rdkafka::util::get_rdkafka_version;
+
+use utils::setup_logger;
+
+mod utils;
 
 async fn produce(brokers: &str, topic_name: &str) {
     let producer: &FutureProducer = &ClientConfig::new()
@@ -12,8 +18,12 @@ async fn produce(brokers: &str, topic_name: &str) {
         .create()
         .expect("Producer creation error");
 
-    let _ = (0..5)
+    // This loop is non blocking: all messages will be sent one after the other, without waiting
+    // for the results.
+    let futures = (0..5)
         .map(|i| async move {
+            // The send operation on the topic returns a future, which will be
+            // completed once the result or failure from Kafka is received.
             let delivery_status = producer
                 .send(
                     FutureRecord::to(topic_name)
@@ -26,15 +36,28 @@ async fn produce(brokers: &str, topic_name: &str) {
                     Duration::from_secs(0),
                 )
                 .await;
+
+            // This will be executed when the result is received.
+            info!("Delivery status for message {} received", i);
             delivery_status
         })
         .collect::<Vec<_>>();
+
+    // This loop will wait until all delivery statuses have been received.
+    for future in futures {
+        info!("Future completed. Result: {:?}", future.await);
+    }
 }
 
 #[tokio::main]
 async fn main() {
+    setup_logger(true, Some(&String::from("debug")));
+
+    let (version_n, version_s) = get_rdkafka_version();
+    info!("rd_kafka_version: 0x{:08x}, {}", version_n, version_s);
+
+    let topic = "hello";
     let brokers = "localhost:9092";
-    let topic = "stream.lifecycle.v1";
 
     produce(brokers, topic).await;
 }
